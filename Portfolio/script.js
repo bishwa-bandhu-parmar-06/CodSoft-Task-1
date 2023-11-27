6b14d85e8d1d81aa91d911b819b205dcdd5fae2a
BISHWA BANDHU PARMAR <bishwabandhuparmar06@gmail.com>
2023-11-27 15:16:26 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,17 @@
+// const projecthover1 = document.querySelector('.project1');
+// const projecthover2 = document.querySelector('.project2');
+// const projecthover3 = document.querySelector('.project3');
+// const projecthover4 = document.querySelector('.project4');
+// const descriptioneff = document.querySelector('.description');
+// projecthover1.addEventListener('mouseover', () => {
+//     projecthover1.classList.add('description');
+// });
+// projecthover2.addEventListener('mouseover', () => {
+//     projecthover1.classList.add('description');
+// });
+// projecthover3.addEventListener('mouseover', () => {
+//     projecthover1.classList.add('description');
+// });
+// projecthover4.addEventListener('mouseover', () => {
+//     projecthover4.classList.add('description');
+// });
